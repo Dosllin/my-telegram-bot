687cc8e0dc25797fb9d1fad5d1ac539bd31f5d76
Dosllin <89354375+Dosllin@users.noreply.github.com>
2025-02-26 22:49:48 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("🎉 Я работаю 24/7!")
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
